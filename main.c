bed412bccd0635f302deefc23db1d32e308ea2dd
Indradhanush Gupta <indradhanush.gupta@gmail.com>
2017-05-30 17:42:05 -0400

    Code refactor
    - Handle error from execvp
    - Refactor make_input and expand_argv with cleaner code

--- main.c
+++ main.c
@@ -5,33 +5,43 @@
 #include <readline/history.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <math.h>
+
+
+#define MEMORY_CHUNK 64
 
 
 struct input{
-    char *command;
     char **argv;
+    int size_argv;
 };
 
 struct input make_input(char *);
 
-void * expand_argv(char **, int, int);
+void expand_argv(struct input *);
 
 int main() {
     char *command;
     pid_t child_pid;
     pid_t wait_result;
     int stat_loc;
+    int result_execvp;
     while (1) {
         command = readline("rcsh> ");
-        struct input input = make_input(command);
+        struct input inp = make_input(command);
+        if (inp.argv == NULL) {
+            continue;
+        }
 
         child_pid = fork();
         
         // The child process
         if (child_pid == 0) {
-            execvp(input.command, input.argv);
-            exit(1);
+            result_execvp = execvp(inp.argv[0], inp.argv);
+            if (result_execvp == -1) {
+                printf("%s - An error occurred.\n", inp.argv[0]);
+                exit(1);
+            }
+            exit(0);
         } else {
             wait_result = waitpid(child_pid, &stat_loc, WUNTRACED);
         }
@@ -41,42 +51,48 @@ int main() {
 
 struct input make_input(char *line) {
     int word_count = 0;
-    int block = 1;
+    int block = 4;
     static char *separator = " ";
-    struct input input;
+    struct input inp = {NULL, 0};
 
     char *parsed = strtok(line, separator);
 
     if (parsed == NULL) {
-        // TODO: Return an Error
+        return inp;
     }
-
-    input.command = parsed;
-    input.argv = NULL;
 
     while (parsed != NULL) {
         if (word_count % block == 0) {
-            input.argv = expand_argv(input.argv, word_count, block);
+            expand_argv(&inp);
         }
 
-        input.argv[word_count] = parsed;
+        inp.argv[word_count] = parsed;
         parsed = strtok(NULL, separator);
         word_count++;
     }
     
-    input.argv[word_count] = (char *)0;
-    return input;
+    if (inp.size_argv == word_count) {
+        realloc(inp.argv, sizeof(char *));
+    }
+    inp.argv[word_count] = (char *)0;
+    return inp;
 }
 
-void * expand_argv(char **ptr, int count, int block) {
-    int memory_chunk = sizeof(char *) * block;
+void expand_argv(struct input *inp_ptr) {
+    int new_size_argv;
 
-    if (!ptr) {
-        ptr = malloc(memory_chunk);
-        return ptr;
+    if (!inp_ptr->argv) {
+        inp_ptr->argv = malloc(MEMORY_CHUNK);
+        // TODO: handle error from malloc
+
+        inp_ptr->size_argv = MEMORY_CHUNK;
+        return;
     }
 
-    memory_chunk = pow(2, count) * memory_chunk;
-    ptr = realloc(ptr, memory_chunk);
-    return ptr;
+    new_size_argv = 2 * inp_ptr->size_argv;
+
+    inp_ptr->argv = realloc(inp_ptr->argv, new_size_argv);
+    // TODO: handle error from realloc
+
+    inp_ptr->size_argv = new_size_argv;
 }
